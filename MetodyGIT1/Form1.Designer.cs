c02594cd18ebcfebfaa1aa71c27f7e3b481354c8
Lycky_LITH <prochazkal.03@spst.eu>
2021-03-24 13:12:45 +0100

    Metoda1

--- MetodyGIT1/Form1.Designer.cs
+++ MetodyGIT1/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace MetodyGIT1
             this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -76,11 +77,21 @@ namespace MetodyGIT1
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker3);
@@ -100,6 +111,7 @@ namespace MetodyGIT1
         private System.Windows.Forms.DateTimePicker dateTimePicker3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- MetodyGIT1/Form1.cs
+++ MetodyGIT1/Form1.cs
@@ -22,11 +22,24 @@ namespace MetodyGIT1
             if (dt2.Value < min) min = dt2.Value;
             if (dt3.Value < min) min = dt3.Value;
             return min;
+            // Vzkutku krásně napsaná metoda <3
         }
+
+        //Metoda1
+        //Dělá pravý opak jako metoda Nejstarší
+        DateTime nejmladsi(DateTimePicker dt1, DateTimePicker dt2, DateTimePicker dt3)
+        {
+            DateTime max = dt1.Value;
+            if (dt2.Value > max) max = dt2.Value;
+            if (dt3.Value > max) max = dt3.Value;
+            return max;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             
             label1.Text = nejstarsi(dateTimePicker1,dateTimePicker2,dateTimePicker3).ToString("d. M. yyyy");
+            label2.Text = nejmladsi(dateTimePicker1, dateTimePicker2, dateTimePicker3).ToString("d. M. yyyy");
         }
     }
 }
